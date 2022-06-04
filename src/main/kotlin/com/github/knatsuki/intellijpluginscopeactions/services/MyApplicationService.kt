ad37e62f839511cc34e7f9bcea44a06d1bdc66f7
GitHub Action <action@github.com>
2022-06-04 21:53:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/knatsuki/intellijpluginscopeactions/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.knatsuki.intellijpluginscopeactions.services
+
+import com.github.knatsuki.intellijpluginscopeactions.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
